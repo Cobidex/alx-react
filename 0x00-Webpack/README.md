5a1b6415fe6fbe419e9dc9ac83dafaf2837d4b8c
Stephen Obialor <stevobialor@gmail.com>
2023-11-04 14:09:10 +0000

    completion of task 0

---
+++ 0x00-Webpack/README.md
@@ -0,0 +1,4 @@
+# 0x00. Webpack
+
+This directory contains my alx training for usage of webpack for frontend web development.
+webpack is a static bundler for frontend projects that uses a graphical map to put together frontend modules into a single that can be rendered in the browser
---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+  const p1 = $("<p>Holberton Dashboard</p>");
+  const p2 = $("<p>Dashboard data for the students</p>");
+  const p3 = $("<p>Copyright - Holberton School</p>");
+
+  $("body").append(p1);
+  $("body").append(p2);
+  $("body").append(p3);
+});
