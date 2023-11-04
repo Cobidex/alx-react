bf932d2be7f3d1233a4e0b7024cbc7b228dc766b
Stephen Obialor <stevobialor@gmail.com>
2023-11-04 14:47:47 +0000

    make code executable

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,11 +1,9 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-  const p1 = $("<p>Holberton Dashboard</p>");
-  const p2 = $("<p>Dashboard data for the students</p>");
-  const p3 = $("<p>Copyright - Holberton School</p>");
+const p1 = $("<p>Holberton Dashboard</p>");
+const p2 = $("<p>Dashboard data for the students</p>");
+const p3 = $("<p>Copyright - Holberton School</p>");
 
-  $("body").append(p1);
-  $("body").append(p2);
-  $("body").append(p3);
-});
+$("body").append(p1);
+$("body").append(p2);
+$("body").append(p3);
