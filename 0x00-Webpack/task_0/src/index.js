a15d05820d42788147fc508dd788a8f186a7a733
Stephen C Obialor <stevobialor@gmail.com>
2023-11-05 08:19:29 +0100

    avoid conflicts

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,6 @@
 import jQuery from 'jquery';
 
-jQuery.noConflict();
-
-jQuery( document ).ready(function( $ ) {
+jQuery(function($) {
   $("body").append("<p>Holberton Dashboard</p>");
   $("body").append("<p>Dashboard data for the students</p>");
   $("body").append("<p>Copyright - Holberton School</p>");
