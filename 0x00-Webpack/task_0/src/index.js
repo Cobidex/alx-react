f3fb4b0ff31c8f5aeb86b06b6b4a684c0704dc34
Stephen C Obialor <stevobialor@gmail.com>
2023-11-05 08:26:34 +0100

    done

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,6 @@
-import jQuery from 'jquery';
+'use strict';
+import $ from 'jquery';
 
-jQuery(function($) {
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<p>Copyright - Holberton School</p>");
-});
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");
