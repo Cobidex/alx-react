cf61c8e3e5d88beb4c86b80110cb000edcb70a5d
Stephen C Obialor <stevobialor@gmail.com>
2023-11-07 08:02:35 +0100

    create dashboard

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='App-header'>
+        <div id='logo'></div>
+        <h1>School dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
